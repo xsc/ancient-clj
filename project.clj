3cdf6a99ef6cdd19ec2ec6018611079cc4400d22
Yannick Scherer <yannick.scherer@futurice.com>
2021-02-13 14:48:48 +0100

    Version 1.0.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ancient-clj "1.0.0-SNAPSHOT"
+(defproject ancient-clj "1.0.0"
   :description "Maven Version Utilities for Clojure"
   :url "https://github.com/xsc/ancient-clj"
   :license {:name "MIT"
